a5fac3aa5acaae7332decb7cdb661c2b8087f1e7
InagakiTatuya <nkc.inagaki.tatuya@gmail.com>
2015-01-24 14:22:14 +0900

    判定テスト

---
+++ project/Assets/Inagaki_Coll/ObjectCollider.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectCollider : MonoBehaviour {
+
+    [SerializeField]
+    private int m_power = 5;
+
+    private Renderer m_ChildRend;
+
+
+    public Vector2 setPos{ set{ transform.position = value; } }
+    public int getPower{ get{ return m_power;  } }  
+    public int setPower{ set{ m_power = value; } }  
+
+	void Start () {
+	    m_ChildRend = transform.FindChild("Model").renderer;
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D c) {
+        //強さの判定
+        int val = c.GetComponent<ObjectCollider>().getPower - m_power;
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -勝ち 
+        if(val > 0) {
+            Debug.Log(""+gameObject.name+":かち");
+            m_ChildRend.material.color = Color.yellow;
+            return;
+        }
+        
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -まけ 
+        if(0 > val) {
+            Debug.Log(""+gameObject.name+":まけ");
+            m_ChildRend.material.color = Color.magenta;
+            return;
+        }
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -あいこ
+        if(val == 0){
+            Debug.Log(""+gameObject.name+":あいこ");
+            m_ChildRend.material.color = Color.grey;
+            return;
+        }
+    }
+}
---
+++ project/Assets/Inagaki_Coll/test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    GameObject a, b;
+
+	void Start () {
+	    a = GameObject.Find("A");
+	    b = GameObject.Find("B");
+	}
+	
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.A)) {
+            a.GetComponent<ObjectCollider>().setPos = new Vector2(0,1);
+            b.GetComponent<ObjectCollider>().setPos = new Vector2(0,0);
+            Debug.Log("----------------------");
+        }
+        if(Input.GetKeyDown(KeyCode.S)) {
+            a.GetComponent<ObjectCollider>().setPos = new Vector2(0,5);
+            b.GetComponent<ObjectCollider>().setPos = new Vector2(0,0);
+            Debug.Log("----------------------");
+        }
+
+	}
+}
